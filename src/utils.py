dd003b137cdd4a9bbe0262a9e0f7152e97942bf5
Fantosism <businessspamfolder@gmail.com>
2026-02-02 17:44:48 -0800

    Add formatted STDOut on run main.sh

--- src/utils.py
+++ src/utils.py
@@ -3,14 +3,20 @@ import shutil
 
 def copy_source_to_target(source, target):
     if os.path.exists(target):
+        print(f"Removing existing directory: {os.path.relpath(target)}/")
         shutil.rmtree(target)
 
+    print(f"Creating fresh directory: {os.path.relpath(target)}/")
     os.mkdir(target)
 
     items = os.listdir(source)
     for item in items:
+        source_path = os.path.join(source, item)
+        target_path = os.path.join(target, item)
         is_file = os.path.isfile(os.path.join(source, item))
         if is_file:
-            shutil.copy(os.path.join(source, item), target)
+            print(f"Copying file: {os.path.relpath(source_path)} -> {os.path.relpath(target_path)}")
+            shutil.copy(source_path, target)
         else:
-            copy_source_to_target(os.path.join(source, item), os.path.join(target, item))
+            print(f"Entering directory: {item}")
+            copy_source_to_target(source_path, target_path)
